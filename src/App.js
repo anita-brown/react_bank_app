a3087e9843a9dd8117d9d3474625ae14d09ef7b2
anita-brown <efeaniita@gmail.com>
2022-11-30 19:59:24 +0100

    chore: created assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-  
+    <div className="">
+      <p>This is <mark>goom</mark></p>
+    
     </div>
+    
   );
 }
 
